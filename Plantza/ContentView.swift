094f95cf0005b27ccef94af11020670c5e646dc3
Satya Prakash Sahu <satyasahu79@gmail.com>
2021-09-17 13:23:30 +0530

    Initial Commit

---
+++ Plantza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plantza
+//
+//  Created by Satya Prakash Sahu on 17/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Plantza/PlantzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantzaApp.swift
+//  Plantza
+//
+//  Created by Satya Prakash Sahu on 17/09/21.
+//
+
+import SwiftUI
+
+@main
+struct PlantzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
